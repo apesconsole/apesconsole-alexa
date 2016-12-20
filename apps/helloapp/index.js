94f4c4f10c523ddc3c7d6d68aeb5a09ff2c3f8b1
apesconsole <apesconsole@gmail.com>
2016-12-20 16:56:42 -0600

    app added

--- apps/helloapp/index.js
+++ apps/helloapp/index.js
@@ -1,19 +1,18 @@
 /*
 	Apes's Console
 */
-var logger = require("logging_component");
-var alexa = require('alexa-app');
-
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
+var alexa = require('alexa-app');
 
 // Define an alexa-app
-var app = new alexa.app('helloworld');
+var app = new alexa.app('helloapp','myEndpointName');
 app.launch(function(req,res) {
 	res.say("Hello World!!");
 });
 app.intent('sampleIntent', function(request,response) {
     response.say("Hi There!");
 });
+
 module.exports = app;
 
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var AlexaAppServer = require('alexa-app-server');
-AlexaAppServer.start({
+
+AlexaAppServer.start( {
     server_root:__dirname,     // Path to root 
     public_html:"public",      // Static content 
-    app_dir:"hello-app",            // Where alexa-app modules are stored 
-    app_root:"/",              // Service root 
-    port:3001                    // What port to use, duh 
-});
+    app_dir:"apps",       // Where alexa-app modules are stored 
+    app_root:"/",  
+	port:3001
+	// Use preRequest to load user data on each request and add it to the request json.
+	// In reality, this data would come from a db or files, etc.
+	,preRequest: function(json,req,res) {
+		console.log("preRequest fired");
+		json.userDetails = { "name":"Bob Smith" };
+	}
+	// Add a dummy attribute to the response
+	,postRequest: function(json,req,res) {
+		json.dummy = "text";
+	}
+} );
